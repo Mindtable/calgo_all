287a91f67215de77bf5c4cc54f87841d1f6358ea
Mindtable <mtble@mail.ru>
2021-09-29 16:38:21 +0300

    added lab4b

---
+++ lab4/lab4b/cmake-build-debug/CMakeFiles/lab4a.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab4a.dir/main.cpp.obj"
+  "lab4a.exe"
+  "lab4a.exe.manifest"
+  "lab4a.lib"
+  "lab4a.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lab4a.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ lab4/lab4e/cmake-build-debug/CMakeFiles/lab4e.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab4e.dir/main.cpp.obj"
+  "lab4e.exe"
+  "lab4e.exe.manifest"
+  "lab4e.lib"
+  "lab4e.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lab4e.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
