916e29a0ba88e049139463129f50e42ec1d211ae
Mindtable <mtble@mail.ru>
2021-09-29 09:42:30 +0300

    added lab4d

---
+++ lab4/lab4d/cmake-build-debug/CMakeFiles/lab4d.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab4d.dir/main.cpp.obj"
+  "lab4d.exe"
+  "lab4d.exe.manifest"
+  "lab4d.lib"
+  "lab4d.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lab4d.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- lab4/lab4d/main.cpp
+++ lab4/lab4d/main.cpp
@@ -1,6 +1,42 @@
 #include <iostream>
+#include <vector>
+#include <string>
 
+#define ll long long
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::vector<ll> stack;
+    ll stack_size = 0;
+    for (ll j = 0; j < 1007; j++){
+        stack.push_back(-1);
+    }
+    freopen("postfix.in", "r", stdin);
+    freopen("postfix.out", "w", stdout);
+    std::string input;
+    while (std::cin >> input){
+        if (input == "+") {
+            ll b_oper = stack[stack_size - 1];
+            ll a_oper = stack[stack_size - 2];
+            stack_size -= 1;
+            stack[stack_size - 1] = a_oper + b_oper;
+        }
+        else if (input == "-") {
+            ll b_oper = stack[stack_size - 1];
+            ll a_oper = stack[stack_size - 2];
+            stack_size -= 1;
+            stack[stack_size - 1] = a_oper - b_oper;
+        }
+        else if (input == "*") {
+            ll b_oper = stack[stack_size - 1];
+            ll a_oper = stack[stack_size - 2];
+            stack_size -= 1;
+            stack[stack_size - 1] = a_oper * b_oper;
+        }
+        else{
+            stack[stack_size] = std::stoi(input);
+            stack_size++;
+        }
+    }
+    std::cout << stack[0];
+
     return 0;
 }
